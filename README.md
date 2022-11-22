345bfb94b08290c8bbd032eb0255711c23432d7f
zhanglei5757 <1315500916@qq.com>
2022-11-22 16:22:23 +0800

    修复了了图片打印问题

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 #### 介绍
 
 针对打印机打印时候存在一些div无法被打印出来，只能先将其转为图片，再打印,
-采用了两个第三方插件库html2canvas，vue-easy-print结合，
+采用了两个第三方插件库html2canvas，
 
 #### 软件架构
 
@@ -20,13 +20,5 @@
 1. 对打印模块进行了封装，只需要引入组件printImage模块即可
 2. 针对打印两个重要的值，一个是body.document,我们采用ref（divRef）,将ref传过来，一个事内置函数clickImage,也是通过组件的ref,由父组件调用子组件的方法触发
 3. 使用很方便
-  `
-  import PrintImage from "@/components/printImage.vue";
-  <PrintImage :divRef="divRef" ref="PrintRef"/>;
-  const divRef = ref();
-  const PrintRef = ref();
-  const clickImage = () => {
-    PrintRef.value.clickImage()
-  };
-  `
+   `import PrintImage from "@/components/printImage.vue"; <PrintImage :divRef="divRef" ref="PrintRef"/>; const divRef = ref(); const PrintRef = ref(); const clickImage = () => { PrintRef.value.clickImage() };`
 4. 采用组件化的方式是为了让公司成员更方便地使用
--- src/components/printImage.vue
+++ src/components/printImage.vue
@@ -1,38 +1,38 @@
 <template>
-  <vue-easy-print ref="printRef" tableShow style="display: none">
-    <img :src="imgSrc" />
-  </vue-easy-print>
+  <PrintCom ref="printRef" tableShow>
+    <img :src="imgSrc" style="width: 1280px" />
+  </PrintCom>
 </template>
 <script setup>
-import { ref, onMounted, nextTick } from "vue";
-import html2canvas from "html2canvas";
-import vueEasyPrint from "vue-easy-print";
+import { ref, onMounted, nextTick } from 'vue'
+import html2canvas from 'html2canvas'
+import PrintCom from './printCom.vue'
 const props = defineProps({
   divRef: {
-    required: true,
-  },
-});
-const printRef = ref();
-const imgSrc = ref();
+    required: true
+  }
+})
+const printRef = ref()
+const imgSrc = ref()
 const clickImage = () => {
   html2canvas(props.divRef)
     .then((canvas) => {
-      let dataURL = canvas.toDataURL("image/png", 1);
-      imgSrc.value = dataURL;
+      let dataURL = canvas.toDataURL('image/png', 1)
+      imgSrc.value = dataURL
       nextTick(() => {
-        printRef.value.print();
-      });
+        printRef.value.print()
+      })
     })
     .catch((e) => {
-      console.log("不知道为什么生成错误了");
+      console.log('不知道为什么生成错误了')
     })
     .finally(() => {
-      console.log("已完成");
-    });
-};
+      console.log('已完成')
+    })
+}
 defineExpose({
-  clickImage,
-});
+  clickImage
+})
 
-onMounted(() => {});
+onMounted(() => {})
 </script>
